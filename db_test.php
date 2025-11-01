2b93f482dd12e5b706be5741d49d8baf24110fb8
JiHeon <jiheon1992@gmail.com>
2025-11-01 22:04:00 +0900

    test

--- db_test.php
+++ db_test.php
@@ -1,3 +1,10 @@
 <?php 
-    echo "s";
+    $mysqli = new mysqli("localhost","jiheon1992","!!aA165165","THXDEAL_DB");
+
+    if($mysqli -> connect_error){
+        echo "fail".$mysqli->connect_error;
+        exit();
+    };
+
+    echo "success";
 ?>
