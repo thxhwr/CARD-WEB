30f74abe22113732e22ed881d522a1a146c11fcb
JiHeon <jiheon1992@gmail.com>
2025-11-01 21:56:55 +0900

    test

--- db_test.php
+++ db_test.php
@@ -1,5 +1,5 @@
 <?php 
-    $mysqli = new mysqli("localhost","jiheon","!!aA165165","THXDEAL_DB");
+    $mysqli = new mysqli("localhost","jiheon1992","!!aA165165","THXDEAL_DB");
 
     if($mysqli -> connect_errno){
         echo "fail".$mysqli->connect_error;
