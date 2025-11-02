b20b7e294952a7391d11a84f5caf045eb2eade1a
JiHeon <jiheon1992@gmail.com>
2025-11-02 16:24:40 +0900

    test

--- db_test.php
+++ db_test.php
@@ -3,18 +3,10 @@ ini_set ('error_reporting', E_ALL);
 ini_set ('display_errors', '1');
 error_reporting (E_ALL|E_STRICT);
 
-
-$host = '72.60.237.149';  // MySQL 서버 IP
-$user = 'thxdeal';     // MySQL 사용자
-$pass = 'dealThx11223@#';
-$db   = 'THXDEAL_DB';
-$port = 37722;              // MariaDB 포트
-
 $mysqli = mysqli_init();
 
 // SSL 설정
-mysqli_ssl_set(
-    $mysqli,
+$mysqli->ssl_set(
     '/home/thxdeal/mysql_certs/client-key.pem',
     '/home/thxdeal/mysql_certs/client-cert.pem',
     '/home/thxdeal/mysql_certs/ca.pem',
@@ -22,11 +14,23 @@ mysqli_ssl_set(
     NULL
 );
 
-// SSL 옵션과 함께 연결
-if (!mysqli_real_connect($mysqli, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
-    die('MySQL 연결 실패: (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+// 서버 인증서 검증 옵션 설정
+$mysqli->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
+
+// 실제 연결
+if (!$mysqli->real_connect(
+    '72.60.237.149', 
+    'thxdeal', 
+    'dealThx11223@#', 
+    'THXDEAL_DB', 
+    37722, 
+    NULL, 
+    MYSQLI_CLIENT_SSL
+)) {
+    die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
 
-echo 'MySQL SSL 연결 성공!';
+echo "SSL 연결 성공";
+
 
 ?>
