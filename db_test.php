4aa2103b551ac301e7ae26193a12c6a519954d60
JiHeon <jiheon1992@gmail.com>
2025-11-02 14:55:04 +0900

    test

--- db_test.php
+++ db_test.php
@@ -25,14 +25,4 @@ if ($mysqli->connect_error) {
     die("DB 연결 실패: " . $mysqli->connect_error);
 }
 echo "✅ SSL DB 연결 성공!";
-
-    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    try {
-        $mysqli = new mysqli("72.60.237.149", "thxdeal", "dealThx11223@#", "THXDEAL_DB","37722");
-        echo "✅ DB 연결 성공!";
-    } catch (mysqli_sql_exception $e) {
-        echo "❌ DB 연결 실패: " . $e->getMessage();
-    }
-
-    echo "success";
 ?>
