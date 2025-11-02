17326da0cc64b3ce06c0535479b69a7cdc31f056
JiHeon <jiheon1992@gmail.com>
2025-11-02 14:46:05 +0900

    test

--- db_test.php
+++ db_test.php
@@ -1,10 +1,12 @@
 <?php 
-    $mysqli = new mysqli("72.60.237.149","thxdeal","dealThx11223@#","THXDEAL_DB", "37722");
 
-    if($mysqli->connect_errno){
-        die("fail".$mysqli->connect_error);
-        exit();
-    };
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    try {
+        $mysqli = new mysqli("72.60.237.149", "thxdeal", "dealThx11223@#", "THXDEAL_DB","37722");
+        echo "✅ DB 연결 성공!";
+    } catch (mysqli_sql_exception $e) {
+        echo "❌ DB 연결 실패: " . $e->getMessage();
+    }
 
     echo "success";
 ?>
