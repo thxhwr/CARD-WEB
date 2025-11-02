0416ddc51772746e55cb2612965c78d2e14b1d2a
JiHeon <jiheon1992@gmail.com>
2025-11-02 14:32:26 +0900

    test

--- db_test.php
+++ db_test.php
@@ -1,5 +1,5 @@
 <?php 
-    $mysqli = new mysqli("localhost","jiheon1992","!!aA165165","THXDEAL_DB");
+    $mysqli = new mysqli("72.60.237.149","thxdeal","dealThx11223@#","THXDEAL_DB");
 
     if($mysqli->connect_errno){
         die("fail".$mysqli->connect_error);
