61e1047894c088591f71378d43d1d6c2c6cd9a1c
JiHeon <jiheon1992@gmail.com>
2025-11-02 15:03:49 +0900

    ㅅㄷㄴㅅ

--- db_test.php
+++ db_test.php
@@ -10,18 +10,7 @@ mysqli_ssl_set(
     NULL, NULL
 );
 
-mysqli_real_connect(
-    $mysqli,
-    "72.60.237.149",                    // DB 서버 IP
-    "thxdeal",                    // 사용자명
-    "dealTHX11223@#",                   // 비밀번호
-    "THXDEAL_DB",                 // 데이터베이스명
-    37722,                         // 포트
-    NULL,
-    MYSQLI_CLIENT_SSL
-);
-
-if (!mysqli_real_connect($mysqli, '1.1.1.1', 'jiheon1992', '비밀번호', 'DB이름', 1234, NULL, MYSQLI_CLIENT_SSL)) {
+if (!mysqli_real_connect($mysqli, '72.60.237.149', 'thxdeal', 'dealThx11223@#', 'THXDEAL_DB', 37722, NULL, MYSQLI_CLIENT_SSL)) {
     die('Connect Error (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
 }
 
