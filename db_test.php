69dbaeb2e0901920f18ee21cf536ab3348deed91
JiHeon <jiheon1992@gmail.com>
2025-11-02 14:34:27 +0900

    test

--- db_test.php
+++ db_test.php
@@ -1,5 +1,5 @@
 <?php 
-    $mysqli = new mysqli("72.60.237.149","thxdeal","dealThx11223@#","THXDEAL_DB");
+    $mysqli = new mysqli("72.60.237.149","thxdeal","dealThx11223@#","THXDEAL_DB", "37722");
 
     if($mysqli->connect_errno){
         die("fail".$mysqli->connect_error);
