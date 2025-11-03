8d32ecbc76c66135d0d8dea977b4503f11a4be21
Jiheon <jiheon1992@gmail.com>
2025-11-03 15:41:46 +0900

    test

--- index.php
+++ index.php
@@ -29,7 +29,9 @@
           </ul>
           <p class="desc">지금 발급하면 <b>3~5일</b> 이내에 <br>안전하게 배송해드려요</p>
         </article>
-        <button class="cta" type="button">오프라인 카드 발급 신청 <img src="/assets/icons/btn-next-arrow-right-w@2x.png" width="16px" height="16px"></button>
+        <button class="cta" type="button">오프라인 카드 발급 신청 
+          <img src="/assets/icons/btn-next-arrow-right-w@2x.png" width="16px" height="16px" style="display:inline;top:20px">
+        </button>
       </section>
     </main>
     <?php include __DIR__ . "/footer.php"; ?>
