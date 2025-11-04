3bd9a1e5dca041da3f5189c6b1f8724d3a82b2e9
Jiheon <jiheon1992@gmail.com>
2025-11-04 14:12:09 +0900

    test

--- shop.php
+++ shop.php
@@ -22,31 +22,52 @@
                 <img src="/assets/img/img-main-event-sample01.png" alt="혜택 배너">
             </div>
 
-            <!-- 2) 카테고리 아이콘 -->
-            <div class="shop-categories">
-                <button class="cat is-active">전체</button>
-                <button class="cat">건강</button>
-                <button class="cat">뷰티</button>
-                <button class="cat">리빙</button>
-                <button class="cat">식품</button>
-                <button class="cat">여행</button>
-            </div>
+            <div class="shop-best">
+                <div class="shop-categories">
+                    <button class="cat is-active">
+                        <img src="/assets/icons/icon-all.svg" class="ico">
+                        <span>전체</span>
+                    </button>
 
-            <!-- 3) 상품 리스트 -->
-            <div class="shop-grid">
-                <div class="product">
-                <img src="/assets/img/prod1.png" alt="">
-                <p class="name">피넛츠 프렌즈컵 여행용 기프트 텀블러</p>
-                <div class="price"><span class="discount">45%</span> 12,400원</div>
+                    <button class="cat">
+                        <img src="/assets/icons/icon-health.svg" class="ico">
+                        <span>건강</span>
+                    </button>
+
+                    <button class="cat">
+                        <img src="/assets/icons/icon-life.svg" class="ico">
+                        <span>생활</span>
+                    </button>
+
+                    <button class="cat">
+                        <img src="/assets/icons/icon-beauty.svg" class="ico">
+                        <span>뷰티</span>
+                    </button>
+
+                    <button class="cat">
+                        <img src="/assets/icons/icon-food.svg" class="ico">
+                        <span>식품</span>
+                    </button>
+
+                    <button class="cat">
+                        <img src="/assets/icons/icon-car.svg" class="ico">
+                        <span>자동차</span>
+                    </button>
                 </div>
 
-                <div class="product">
-                <img src="/assets/img/prod2.png" alt="">
-                <p class="name">아모레퍼시픽 럭키 에디션 세트</p>
-                <div class="price"><span class="discount">20%</span> 9,200원</div>
+                <div class="shop-grid">
+                    <div class="product">
+                        <img src="/assets/img/prod1.png" alt="">
+                        <p class="name">피넛츠 프렌즈컵 여행용 기프트 텀블러</p>
+                        <div class="price"><span class="discount">45%</span> 12,400원</div>
+                    </div>
+                    <div class="product">
+                        <img src="/assets/img/prod2.png" alt="">
+                        <p class="name">아모레퍼시픽 럭키 에디션 세트</p>
+                        <div class="price"><span class="discount">20%</span> 9,200원</div>
+                    </div>
                 </div>
 
-                <!-- ...복제해서 나열... -->
             </div>
 
             </section>
