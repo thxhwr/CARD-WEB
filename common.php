f676b0a67f1c67457133846a50e6f6f91d71ad95
Jiheon <jiheon1992@gmail.com>
2025-12-18 16:04:52 +0900

    ttest

--- common.php
+++ common.php
@@ -2,7 +2,7 @@
 session_start();
 
 // 자동 로그인 처리
-require_once __DIR__ . "/remember_login.php";
+//require_once __DIR__ . "/remember_login.php";
 
 // 로그인 여부
 $isLogin = isset($_SESSION['user_id']);
