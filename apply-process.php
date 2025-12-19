85b08fdb7229ada53a52e36029b84cf6ce716505
Jiheon <jiheon1992@gmail.com>
2025-12-20 00:28:19 +0900

    ㅅㄷㄴㅅ

--- apply-process.php
+++ apply-process.php
@@ -9,21 +9,15 @@ $addr           = $_POST['address'] ?? '';
 $addr_detail    = $_POST['address_detail'] ?? '';
 
 $address = $zipcode . ' ' . $addr . ' ' . $addr_detail;
-// 간단 유효성 검증
-// if ($id === '' || $pw === '') {
-//     header('Location: /login.php?error=1');
-//     exit;
-// }
 
-// API로 보낼 데이터 (필드명은 API 문서에 맞게 수정)
 $postFields = [
-    'accountNo' => $_SESSION['user_No'],
+'accountNo' => $_SESSION['user_No'],
   'referrerAccountNo' => $referral,
   'name' => $name,
   'phone' => $phone,
   'address' => $address,
 ];
-print_r($postFields);
+
 $ch = curl_init('https://api.thxdeal.com/api/member/testMemberInsert.php');
 curl_setopt_array($ch, [
     CURLOPT_POST => true,
@@ -43,31 +37,7 @@ curl_close($ch);
 $data = json_decode($response, true);
 print_r($data);
 // if ($data['resCode'] == "0") {
-//     if (!empty($_POST['remember_me'])) {
-//         $lifetime = 60 * 60 * 24 * 30;
-//     } else {
-//         $lifetime = 0;
-//     }
-//     session_set_cookie_params([
-//         'lifetime' => $lifetime,
-//         'path'     => '/',
-//         'secure'   => isset($_SERVER['HTTPS']), 
-//         'httponly' => true,
-//         'samesite' => 'Lax',
-//     ]);
-//     session_start();
-
-//     $_SESSION['user_Status'] = $data['data']['status'];
-//     $_SESSION['user_No'] = $data['data']['accountNo'] ?? null;
-//     $_SESSION['user_Id']    = $data['data']['userId'] ?? null;
-
-
-//     session_regenerate_id(true);
-
-//     header('Location: /index.php');
-//     exit;
+//   
 // } else {
 
-//     header('Location: /login.php?error=1');
-//     exit;
 // }
