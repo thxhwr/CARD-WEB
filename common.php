297dd3e5ce4edcd8fc6af24db84849e4a410bcb8
Jiheon <jiheon1992@gmail.com>
2025-12-22 20:33:39 +0900

    test

---
+++ common.php
@@ -0,0 +1,4 @@
+<?php
+    session_start()l
+    $isLogin == $_SESSION['user_Id'];
+?>
--- index.php
+++ index.php
@@ -4,7 +4,6 @@
 <head>
   <?php 
     include __DIR__ . "/head.php"; 
-    session_start();
   ?>
 </head>
 <style>
