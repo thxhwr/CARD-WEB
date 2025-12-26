8717e374644341d9e8b07c69582b197ad5aef3b1
Jiheon <jiheon1992@gmail.com>
2025-12-26 15:00:23 +0900

    test

--- admin/apply-list.php
+++ admin/apply-list.php
@@ -10,7 +10,6 @@ $postFields = [
     'list' => $limit,
 ];
 
-print_r($postFields);
 $ch = curl_init('https://api.thxdeal.com/api/member/testMemberAppList.php');
 curl_setopt_array($ch, [
   CURLOPT_POST           => true,
