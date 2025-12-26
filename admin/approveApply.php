52622564b8057f6f207d4d32a9e911b7a1fd8e5b
Jiheon <jiheon1992@gmail.com>
2025-12-26 18:46:25 +0900

    test

--- admin/approveApply.php
+++ admin/approveApply.php
@@ -12,7 +12,7 @@ if (!$applyId) {
 }
 
 $postFields = [
-    'applyId' => $applyId
+    'applyId' => $applyId,
 ];
 
 $ch = curl_init('https://api.thxdeal.com/api/member/testMemberInsertFirst.php');
