c2e817fa5c3024091e3fd1d4b25b3be295a05df1
Jiheon <jiheon1992@gmail.com>
2026-01-09 13:25:39 +0900

    ㅅㄷㄴㅅ

--- point-history-more.php
+++ point-history-more.php
@@ -76,7 +76,6 @@ while ($maxTry-- > 0) {
 
   $tryPage++; // 다음 페이지 시도
 }
-echo "<!-- rawCount=" . count($rawItems) . " filteredCount=" . count($items) . " io={$io} page={$page} -->";
 
 if (empty($items)) exit;
 foreach ($items as $row) {
