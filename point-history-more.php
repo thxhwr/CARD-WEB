dbdae50697a8abe2a7a5bc12877fbc1e5a1a5be2
Jiheon <jiheon1992@gmail.com>
2026-01-09 13:24:22 +0900

    ㅅㄷㄴㅅ

--- point-history-more.php
+++ point-history-more.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
 header('Content-Type: text/html; charset=UTF-8');
 require_once __DIR__ . "/auth.php";
 
