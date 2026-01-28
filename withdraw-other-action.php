4bcdcef66ae283ff891c4499c45e3a587aa0d4c2
Jiheon <jiheon1992@gmail.com>
2026-01-28 15:04:29 +0900

    ㅅㄷㄴㅅ

--- withdraw-other-action.php
+++ withdraw-other-action.php
@@ -38,8 +38,7 @@ $postFields = [
   'accountNo'   => $_SESSION['user_No'],
   'amount'     => $amount,
 ];
-print_r($postFields);
-exit;
+
 // 예시: cURL
 $ch = curl_init('https://api.thxdeal.com/api/member/memberWithdraw.php');
 curl_setopt_array($ch, [
